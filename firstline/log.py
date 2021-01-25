661de4a625749ef53bf3fbe24a6aab47e38250f1
bezaban <paul@snowcrashed.net>
2021-01-25 20:59:47 +0100

    change signature to include logname, consolidate debug into one function

--- firstline/log.py
+++ firstline/log.py
@@ -2,29 +2,23 @@
 
 import logging
 
-def initlog(debug=False):
+def setuplog(logname, debug=False):
     """Initializes logging"""
 
-    log = logging.getLogger(__package__)
+    log = logging.getLogger(logname)
 
     formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
     filehandler = logging.FileHandler(__package__.split('.')[0]+'.log')
     filehandler.setFormatter(formatter)
+
     log.addHandler(filehandler)
     log.setLevel(logging.INFO)
 
+    if debug:
+        streamhandler = logging.StreamHandler()
+        streamhandler.setLevel(logging.DEBUG)
+        streamhandler.setFormatter(formatter)
+        log.addHandler(streamhandler)
+        log.setLevel(logging.DEBUG)
+
     return log
-
-def enabledebuglog():
-    """Enables debug and stdout streamhandler"""
- 
-    log = logging.getLogger(__package__)
-    #formatter = logging.Formatter('[%(asctime)s:%(name)s][%(levelname)s] %(message)s')
-    formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
-
-    streamhandler = logging.StreamHandler()
-    streamhandler.setLevel(logging.DEBUG)
-    streamhandler.setFormatter(formatter)
-
-    log.addHandler(streamhandler)
-    log.setLevel(logging.DEBUG)
