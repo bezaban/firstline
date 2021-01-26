faaca6a764dfdd9f376d878cb32620a2c9746264
bezaban <paul@snowcrashed.net>
2021-01-26 17:34:20 +0100

    consolidating into one file

--- firstline/firstline.py
+++ firstline/firstline.py
@@ -89,3 +89,30 @@ class ConfigHandler:
         with open(self.configfile, 'w') as outfile:
             json.dump(self.config, outfile, indent=4)
             outfile.close()
+
+def setuplog(logname, debug=False):
+    """Initializes logging"""
+
+    log = logging.getLogger()
+
+    formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S')
+
+    filehandler = logging.FileHandler(logname.split('.')[0]+'.log')
+    filehandler.setFormatter(formatter)
+
+    log.addHandler(filehandler)
+    log.setLevel(logging.INFO)
+
+    if debug:
+        
+        streamhandler = logging.StreamHandler()
+        streamhandler.setFormatter(formatter)
+        
+        streamhandler.setLevel(logging.DEBUG)
+
+        log.addHandler(streamhandler)
+        log.setLevel(logging.DEBUG)
+        log.debug('Debug logging: %s', debug)
+
+    return log
+
