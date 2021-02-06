893cc3f5efec64d483cfe3b544b3d1488f7d47c3
bezaban <paul@snowcrashed.net>
2021-02-06 13:36:04 +0100

    extend test a little

--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -9,7 +9,11 @@ class test_firstline(unittest.TestCase):
     def test_configure_log_from_config(self):
         import logging
         from logging.config import dictConfig
-        logging.config.dictConfig(firstline.helpers.getlogconfig())
+        logging.config.dictConfig(firstline.helpers.getlogconfig('test.log', True))
+        log = logging.getLogger() 
+        
+        log.info('Testing info log level')
+        log.debug('Testing debug log level')
 
 if __name__ == '__main__':
     unittest.main()
