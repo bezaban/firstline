5cc908cecf93e8bce44cde4becab8e320d348c41
bezaban <paul@snowcrashed.net>
2021-02-06 12:57:48 +0100

    add test case for dictConfig

--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -1,10 +1,15 @@
 import unittest
 import firstline 
 
-class testFl(unittest.TestCase):
+class test_firstline(unittest.TestCase):
     
     def test_basic(self):
         self.assertEqual(5, 5)
+
+    def test_configure_log_from_config(self):
+        import logging
+        from logging.config import dictConfig
+        logging.config.dictConfig(firstline.helpers.getlogconfig())
 
 if __name__ == '__main__':
     unittest.main()
