0bf5692fa9b3879a407b6ad457c57d8c743403d2
bezaban <paul@snowcrashed.net>
2021-08-02 20:40:59 +0200

    Revert "implicitly declare namespace package"
    This reverts commit 4411a6e0a6427c1ca52090c95f83871d42c59534.

--- firstline/__init__.py
+++ firstline/__init__.py
@@ -1,5 +1,3 @@
-"""Opinionated collection of reusable functions and classes"""
-#from .helpers import helpers
-#from .pidfile import Pidfile
-#from .confighandler import ConfigHandler
-__import__("pkg_resources").declare_namespace(__name__)
+from .helpers import helpers
+from .pidfile import Pidfile
+from .confighandler import ConfigHandler
