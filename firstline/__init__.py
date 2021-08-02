4411a6e0a6427c1ca52090c95f83871d42c59534
bezaban <paul@snowcrashed.net>
2021-08-02 20:37:43 +0200

    implicitly declare namespace package

--- firstline/__init__.py
+++ firstline/__init__.py
@@ -1,3 +1,5 @@
-from .helpers import helpers
-from .pidfile import Pidfile
-from .confighandler import ConfigHandler
+"""Opinionated collection of reusable functions and classes"""
+#from .helpers import helpers
+#from .pidfile import Pidfile
+#from .confighandler import ConfigHandler
+__import__("pkg_resources").declare_namespace(__name__)
